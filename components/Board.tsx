dbd9fd14ca719ba74a8f375019f999548e80d6b8
= <abhisingh30800@gmail.com>
2023-09-06 12:42:52 +0530

    added zustan and interface

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
-  return <div>Board</div>;
+  useEffect(() => {
+    // get Board
+  }, []);
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div>Rendering all the column</div>}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default Board;
---
+++ type.d.ts
@@ -0,0 +1,27 @@
+interface Board {
+    columns: Map<TypedColumn, Column>
+}
+
+
+type TypedColumn = 'todo' | 'inprogress' | 'done'
+
+
+type Column = {
+    id: TypedColumn,
+    todos: Todo[],
+}
+
+
+interface Todo {
+    $id: string,
+    $createdAt: string,
+    title: string,
+    status: TypedColumn,
+    image?: string,
+}
+
+
+interface Image {
+    bucketId: string,
+    fileId: string,
+}
